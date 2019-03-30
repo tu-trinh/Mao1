0a047acf1deeed8d7aca68fd68e7d66794820f4f
phang-langchen <amyzhong4701@gmail.com>
2019-03-30 09:30:01 -0700

    add Player

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private List<Card> hand;
+
+    public Player() {
+        hand = new ArrayList<Card>();
+    }
+
+
+}
