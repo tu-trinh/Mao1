110e404fc084d1f7d7c159dde7da49d0b56f5290
phang-langchen <amyzhong4701@gmail.com>
2019-03-30 11:27:58 -0700

    added comments to main

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,13 @@ public class Main {
                 deck.add(new Card(st, val));
             }
         }
+
 //        prints out all the cards in the deck
 //        for (Card card : deck) {
 //                System.out.println(card);
 //        }
+
+        //takes in # of players and creates a list of players that size
         System.out.println("Enter how many players: ");
         int numPlayers = myScan.nextInt();
 
@@ -26,22 +29,28 @@ public class Main {
             players.add(new Player());
         }
 
+        //deals cards and displays results
         dealCards(deck, players);
         printCards(deck, players);
 
+        //takes a card from the deck to be the first card of the playDeck (beginning of game)
         playDeck.add(deck.remove((int) (Math.random()* deck.size())));
 
+        //printing check for deck and playDeck
         System.out.println("DECK: " + deck);
         System.out.println("PLAYDECK: " + playDeck);
 
+        //turn for first player, runs until a valid move is played
         while (!turn(players.get(0), playDeck)){}
 
+        //printing check for playDeck and the hands for all the players.
         System.out.println("PLAYDECK: " + playDeck);
         printCards(deck, players);
 
 
     }
 
+    //deals 7 random  cards to each player --> should eventually change the #cards to adjust for #players
     public static void dealCards(List<Card> deckIn, List<Player> playersIn){
         for (int j = 0; j<7; j++)
         {
@@ -52,6 +61,7 @@ public class Main {
 
     }
 
+    //prints out all the cards for all the players
     public static void printCards(List<Card> deckIn, List<Player> playersIn){
         for (Player player: playersIn){
             List<Card> tempHand = player.displayHand();
@@ -59,31 +69,36 @@ public class Main {
         }
     }
 
+    //returns true if the move was valid (checks card validity and whether player has card)
+    //also adds played card to playdeck and removes it from the player's hand
     public static boolean turn(Player playerIn, List<Card> playDeckIn){
+        //takes in what the user would like to play
         Scanner myScan = new Scanner(System.in);
         System.out.println("What would you like to play? Your cards are: " + playerIn.displayHand());
         String cardChosen = myScan.nextLine();
+
+        //checks if card is valid and acts accordingly
         for (int i = 0; i < playerIn.displayHand().size(); i++){
             if (cardChosen.equalsIgnoreCase(playerIn.displayHand().get(i).toString())){ //checking if card is in hand
-                if (isValidMove(playerIn.displayHand().get(i), playDeckIn)){
-                    playDeckIn.add(playerIn.displayHand().get(i));
-                    playerIn.playCard(playerIn.displayHand().get(i));
-//                    System.out.println("Card has been removed and added to playdeck");
-//                    System.out.println(playDeckIn);
-//                    System.out.println(playerIn.displayHand());
+                if (isValidMove(playerIn.displayHand().get(i), playDeckIn)){ //checks if card can be played
+                    playDeckIn.add(playerIn.displayHand().get(i)); //adds card to playDeck
+                    playerIn.playCard(playerIn.displayHand().get(i)); //removes card from player's hand
                     return true;
                 }
-                else{
+                else{ //occurs if card is in hand but suit/value does not match
                     System.out.println("Invalid move. This cannot be played.");
                     return false;
                 }
             }
         }
+        //occurs if card is not in hand at all
         System.out.println("Invalid move. You do not own this card.");
         return false;
 
     }
 
+    //checks if the card chosen and the last card in the play deck have either a matching value or a matching suit
+    //returns true if the card is valid, false if not
     public static boolean isValidMove(Card cardIn, List<Card> playDeckIn){
         if (cardIn.getSuit().equals(playDeckIn.get(playDeckIn.size()-1).getSuit()) || cardIn.getValue().equals(playDeckIn.get(playDeckIn.size()-1).getValue())){
             return true;
