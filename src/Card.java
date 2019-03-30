a9093b1ee1e757957611a6cd536a9f761676405d
Tu Trinh <alinatu.trinh@gmailcom>
2019-03-30 08:26:52 -0700

    Make cards and deck

---
+++ src/Card.java
@@ -0,0 +1,36 @@
+enum Suit {
+    HEARTS,
+    DIAMONDS,
+    SPADES,
+    CLUBS
+}
+
+enum Value {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suitIn, Value valueIn) {
+        suit = suitIn;
+        value = valueIn;
+    }
+
+    public String toString() {
+        return value + " of " + suit;
+    }
+}
