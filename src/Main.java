6728ece37ade53ffbb987914508e4e76a7b92474
ivyc0 <ivycao801@gmail.com>
2019-03-30 10:10:49 -0700

    added dealCards and displaying cards

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
         List<Card> deck = new ArrayList<Card>();
         List<Player> players = new ArrayList<Player>();
 
@@ -15,8 +17,33 @@ public class Main {
         for (Card card : deck) {
                 System.out.println(card);
         }
+        System.out.println("Enter how many players: ");
+        int numPlayers = myScan.nextInt();
+
+        for (int i = 0; i < numPlayers; i++){
+            players.add(new Player());
+        }
+
+        dealCards(deck, players);
+        printCards(deck, players);
+        System.out.println(deck);
     }
 
-
+    public static void dealCards(List<Card> deckIn, List<Player> playersIn){
+        for (int j = 0; j<7; j++)
+        {
+            for (int i = 0; i < playersIn.size(); i++){
+                playersIn.get(i).addCard(deckIn.remove((int) (Math.random()* deckIn.size())));
+            }
+        }
 
     }
+
+    public static void printCards(List<Card> deckIn, List<Player> playersIn){
+        for (Player player: playersIn){
+            List<Card> tempHand = player.displayHand();
+            System.out.println(tempHand);
+        }
+    }
+
+}
