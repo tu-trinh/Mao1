6a1f5f447a3a9735a5e80bfb4d3257326773764a
zhang84maggie <zhang84maggie@gmail.com>
2019-03-30 17:59:52 -0700

    Moved methods in main to a new class GamePlay

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -4,22 +4,11 @@ import java.util.Scanner;
 public class GamePlay {
     static Scanner myScan = new Scanner(System.in);
 
-    //deals 7 random  cards to each player, also changes the # of cards to adjust for #players
+    //deals 7 random  cards to each player --> should eventually change the #cards to adjust for #players
     public static void dealCards(List<Card> deckIn, List<Player> playersIn){
-        int numPlayers = playersIn.size();
-        int numToDeal = 0;
-        if (numPlayers < 4) {
-            numToDeal = 8;
-        } else if (numPlayers == 4) {
-            numToDeal = 7;
-        } else if (numPlayers == 5) {
-            numToDeal = 6;
-        } else if (numPlayers >= 6) {
-            numToDeal = 5;
-        }
-        for (int j = 0; j < numToDeal; j++)
+        for (int j = 0; j<7; j++)
         {
-            for (int i = 0; i < numPlayers; i++){
+            for (int i = 0; i < playersIn.size(); i++){
                 playersIn.get(i).addCard(deckIn.remove((int) (Math.random()* deckIn.size())));
             }
         }
@@ -35,27 +24,12 @@ public class GamePlay {
 
     //returns true if the move was valid (checks card validity and whether player has card)
     //also adds played card to playdeck and removes it from the player's hand
-    public static boolean turn(Player playerIn, List<Player> playersIn, List<Card> playDeckIn, List<Card> deckIn){
+    public static boolean turn(Player playerIn, List<Card> playDeckIn, List<Card> deckIn){
         //takes in what the user would like to play
         System.out.println("What would you like to play? Your cards are: " + playerIn.displayHand());
         String cardChosen = myScan.nextLine();
-        String declaration = "";
-
-        if (cardChosen.equalsIgnoreCase("PENALTY")){
-            castPenalty(deckIn, playersIn);
-            System.out.println("What would you like to play? Your cards are: " + playerIn.displayHand());
-            String cardChosen2 = myScan.nextLine();
-            cardChosen = cardChosen2;
-            if (cardChosen.equalsIgnoreCase("DRAW")){
-                draw(playerIn, deckIn);
-                System.out.println(playerIn.getName() + " has drawn one card. \n");
-                return true;
-            }
-        }
-
         if (cardChosen.equalsIgnoreCase("DRAW")){
             draw(playerIn, deckIn);
-            System.out.println(playerIn.getName() + " has drawn a card from the deck. \n");
             return true;
         }
 
@@ -63,18 +37,8 @@ public class GamePlay {
         for (int i = 0; i < playerIn.displayHand().size(); i++){
             if (cardChosen.equalsIgnoreCase(playerIn.displayHand().get(i).toString())){ //checking if card is in hand
                 if (isValidMove(playerIn.displayHand().get(i), playDeckIn)){//checks if card can be played
-                    // prompts player to say something
-                    System.out.println("Do you have anything to declare?");
-                    declaration = myScan.nextLine();
-
                     playDeckIn.add(playerIn.displayHand().get(i)); //adds card to playDeck
                     playerIn.playCard(playerIn.displayHand().get(i));//removes card from player's hand
-                    // penalizes for forgetting to say mao with one card
-                    if (playerIn.displayHand().size() == 1 && !declaration.equalsIgnoreCase("mao")) {
-                        System.out.println("You've failed to say \"mao.\" You've incurred a one card penalty. \n");
-                        penalty(playerIn, deckIn);
-                        return false;
-                    }
                     // prompts player to say something and displays for all to see
 //                    System.out.println("Say something?");
 //                    String answer = myScan.nextLine();
@@ -83,18 +47,17 @@ public class GamePlay {
 //                    } else {
 //                        System.out.println(playerIn + " says " + answer + ".");
 //                    }
-                    System.out.println(playerIn.getName() + " has played the " + cardChosen +  ".\n");
                     return true;
                 }
                 else{//occurs if card is in hand but suit/value does not match
-                    System.out.println("Invalid move. This cannot be played. You've incurred a one card penalty. \n");
+                    System.out.println("Invalid move. This cannot be played. You've incurred a one card penalty.");
                     penalty(playerIn, deckIn);
                     return false;
                 }
             }
         }
         //occurs if card is not in hand at all
-        System.out.println("Invalid move. You do not own this card. You've incurred a one card penalty. \n");
+        System.out.println("Invalid move. You do not own this card. You've incurred a one card penalty.");
         penalty(playerIn, deckIn);
         return false;
     }
@@ -123,18 +86,6 @@ public class GamePlay {
 
     public static void penalty(Player playerIn, List<Card> deckIn){
         playerIn.addCard(deckIn.remove((int) (Math.random()* deckIn.size())));
-    }
 
-    public static void castPenalty(List<Card> deckIn, List<Player> playersIn){
-        System.out.println("Who should be penalized?");
-        String player = myScan.nextLine();
-        System.out.println("What is the cause of the penalty?");
-        String reason = myScan.nextLine();
-        for (int i = 0; i < playersIn.size(); i++){
-            if (player.equalsIgnoreCase(playersIn.get(i).getName())){
-                playersIn.get(i).addCard(deckIn.remove((int) (Math.random()* deckIn.size())));
-                System.out.println(playersIn.get(i).getName() + " has incurred a one card penalty for " + reason);
-            }
-        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -11,127 +11,75 @@ public class Main {
 //        JLabel emptyLabel = new JLabel("");
 //       emptyLabel.setPreferredSize(new Dimension(175, 100));
 //        mao.setVisible(true);
-        boolean playGame = true;
-        while (play(playGame)) {
-        }
-        System.out.println("Thank you for playing!");
-    }
-
-    public static boolean play(boolean playIn) {
         Scanner myScan = new Scanner(System.in);
         List<Card> deck = new ArrayList<Card>();
         List<Card> playDeck = new ArrayList<Card>();
         List<Player> players = new ArrayList<Player>();
         boolean won = false;
 
-        //this creates the 52 card deck
+        //this creates the 52 card deck and prints it out
         for (Suit st : Suit.values()) {
             for (Value val : Value.values()) {
                 deck.add(new Card(st, val));
             }
         }
 
-        // inquires about pvp or pvc
-        System.out.println("(S)ingle player or (M)ultiplayer?");
-        String playType = myScan.nextLine();
-        if (playType.equalsIgnoreCase("M")) {
-            //takes in # of players and creates a list of players that size
-            System.out.println("Enter how many players: ");
-            int numPlayers = myScan.nextInt();
+//        prints out all the cards in the deck
+//        for (Card card : deck) {
+//                System.out.println(card);
+//        }
 
-            for (int i = 0; i < numPlayers; i++) {
-                //players.add(new Player());
-                System.out.println("Player " + (i + 1) + ", what's your name?");
-                String nextName = myScan.next();
-                players.add(new Player(nextName));
-            }
+        //takes in # of players and creates a list of players that size
+        System.out.println("Enter how many players: ");
+        int numPlayers = myScan.nextInt();
 
-            //deals cards and displays results
-            GamePlay.dealCards(deck, players);
-//        GamePlay.printCards(deck, players);
-
-            //takes a card from the deck to be the first card of the playDeck (beginning of game)
-            playDeck.add(deck.remove((int) (Math.random() * deck.size())));
-
-            //printing check for deck and playDeck
-//        System.out.println("DECK: " + deck);
-            System.out.println("PLAYDECK: " + playDeck);
-
-            while (!won) {
-                for (int i = 0; i < players.size(); i++) {
-                    System.out.println("This is " + players.get(i).getName() + "'s turn.");
-                    System.out.println("PLAYDECK: " + playDeck.get(playDeck.size() - 1));
-                    GamePlay.turn(players.get(i), players, playDeck, deck);
-                    if (players.get(i).getSize() == 0) {
-                        won = true;
-                        System.out.println(players.get(i).getName() + " won!");
-                        System.out.println("Would you like to play again?");
-                        String playAgain = myScan.next();
-                        if (playAgain.equalsIgnoreCase("yes")) {
-                            playIn = true;
-                        } else {
-                            playIn = false;
-                        }
-                        return playIn;
-                    }
-                    if (deck.size() == 0) {
-                        for (int j = 0; j < playDeck.size() - 1; j++) {
-                            deck.add(playDeck.remove(j));
-                        }
-                        System.out.println("Deck has been refilled with playdeck stack.");
-                    }
-                }
-            }
-        } else if (playType.equalsIgnoreCase("S")) {
-            // creates players
-            System.out.println("Player, what is your name?");
-            String playerName = myScan.nextLine();
-            players.add(new Player(playerName));
-            players.add(new Player("Computer"));
-
-            //deals cards and displays results
-            GamePlay.dealCards(deck, players);
-//        GamePlay.printCards(deck, players);
-
-            //takes a card from the deck to be the first card of the playDeck (beginning of game)
-            playDeck.add(deck.remove((int) (Math.random() * deck.size())));
-
-            //printing check for deck and playDeck
-//        System.out.println("DECK: " + deck);
-            System.out.println("PLAYDECK: " + playDeck);
-
-            while (!won) {
-                for (int i = 0; i < players.size(); i++) {
-                    System.out.println("This is " + players.get(i).getName() + "'s turn.");
-                    System.out.println("PLAYDECK: " + playDeck.get(playDeck.size() - 1));
-                    // behold, el sentient computer
-                    if (i == players.size() - 1) {
-                        // computer decisions must be in GamePlay
-                    }
-                    GamePlay.turn(players.get(i), players, playDeck, deck);
-                    if (players.get(i).getSize() == 0) {
-                        won = true;
-                        System.out.println(players.get(i).getName() + " won!");
-                        System.out.println("Would you like to play again?");
-                        String playAgain = myScan.next();
-                        if (playAgain.equalsIgnoreCase("yes")) {
-                            playIn = true;
-                        } else {
-                            playIn = false;
-                        }
-                        return playIn;
-                    }
-                    if (deck.size() == 0) {
-                        for (int j = 0; j < playDeck.size() - 1; j++) {
-                            deck.add(playDeck.remove(j));
-                        }
-                        System.out.println("Deck has been refilled with playdeck stack.");
-                    }
-                }
-            }
+        for (int i = 0; i < numPlayers; i++){
+            //players.add(new Player());
+            System.out.println("Player " + (i+1) +", what's your name?");
+            String nextName = myScan.next();
+            players.add(new Player(nextName));
         }
-        return false;
+
+        //deals cards and displays results
+        GamePlay.dealCards(deck, players);
+        GamePlay.printCards(deck, players);
+
+        //takes a card from the deck to be the first card of the playDeck (beginning of game)
+        playDeck.add(deck.remove((int) (Math.random()* deck.size())));
+
+        //printing check for deck and playDeck
+        System.out.println("DECK: " + deck);
+        System.out.println("PLAYDECK: " + playDeck);
+
+        while (!won) {
+            for (int i = 0; i < players.size(); i ++){
+                System.out.println("This is " + players.get(i).getName() + "'s turn.");
+                System.out.println("PLAYDECK: " + playDeck.get(playDeck.size()-1));
+                GamePlay.turn(players.get(i), playDeck, deck);
+                if (players.get(i).getSize() == 0) {
+                    won = true;
+                    System.out.println(players.get(i).getNaem() + " won!");
+                    break;
+                }
+                if (deck.size() == 0){
+                    for (int j = 0; j < playDeck.size()-1; j++){
+                        deck.add(playDeck.remove(j));
+                    }
+                    System.out.println("Deck has been refilled with playdeck stack.");
+                }
+            }
+
+        }
+
+        //turn for first player, runs until a valid move is played
+//        while (!GamePlay.turn(players.get(0), playDeck, deck)){}
+
+        //printing check for playDeck and the hands for all the players.
+//        System.out.println("PLAYDECK: " + playDeck);
+//        GamePlay.printCards(deck, players);
+
+
     }
+
+
 }
-
-
